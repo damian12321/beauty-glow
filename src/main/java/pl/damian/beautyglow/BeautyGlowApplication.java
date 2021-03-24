bd2b2da89cb99c9f11d94b699b6c43995ad28a9f
Damian Juruś <damianjurus@wp.pl>
2021-03-24 15:58:44 +0100

    First commit

---
+++ src/main/java/pl/damian/beautyglow/BeautyGlowApplication.java
@@ -0,0 +1,13 @@
+package pl.damian.beautyglow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyGlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyGlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/damian/beautyglow/BeautyGlowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.damian.beautyglow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyGlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
